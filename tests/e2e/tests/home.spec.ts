7590b0689e5a35f6fed89a47eebfa3fa2745503e
Matt Carlos <catmarlos@icloud.com>
2023-09-05 18:48:31 -0700

    fix cypress test for progress to dashboard test

--- tests/e2e/tests/home.spec.ts
+++ tests/e2e/tests/home.spec.ts
@@ -12,8 +12,12 @@ describe('The Home Page/Team Select', () => {
   });
 
   it('should let a user select a team and then progress to the dashboard', () => {
-    cy.get('teamDropdown').click();
+    cy.get('#teamDropdown').click();
     // select astros, which corresponds with our schedule and live data
-    cy.get('117').click();
+    cy.get('#117').click();
+
+    cy.findByRole('heading', {
+      name: /Houston Astros/i,
+    }).should('exist');
   });
 });
